c2b6dcbf19c15387e26a7fdbd31cb28b8b2037b2
strday <60084862+strday@users.noreply.github.com>
2020-07-21 22:32:29 +0700

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -5,17 +5,17 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
-# Penguin Prediction App
+# Home Credit Risk Prediction App
 
-This app predicts the **Palmer Penguin** species!
+This app predicts Home Credit Risk!
 
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+Data obtained from the [Kaggle] (https://www.kaggle.com/c/home-credit-default-risk/data).
 """)
 
 st.sidebar.header('User Input Features')
 
 st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+[Example CSV input file](https://raw.githubusercontent.com/strday/penguins-heroku/master/penguins_example.csv)
 """)
 
 # Collects user input features into dataframe
@@ -24,17 +24,13 @@ if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
         sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
+        bill_length_mm = st.sidebar.slider('Payment Rate (USD)', 30,60,40)
+        bill_depth_mm = st.sidebar.slider('Extra Source 1 (USD)', 10,20,15)
+        flipper_length_mm = st.sidebar.slider('Extra Source 1 (USD)', 150,250,200)
+        data = {'bill_length_mm': bill_length_mm,
                 'bill_depth_mm': bill_depth_mm,
                 'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
                 'sex': sex}
         features = pd.DataFrame(data, index=[0])
         return features
